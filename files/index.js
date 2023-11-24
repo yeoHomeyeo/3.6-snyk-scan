ddf09a167e48c248d8ef39835c02c41b0455c8b6
iuolxvpq <luqmannurhakimt@gmail.com>
2023-11-24 23:50:11 +0800

    Ambitious testing

--- files/index.js
+++ files/index.js
@@ -1,12 +1,14 @@
-module.exports.handler = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-    {
-        message: "Welcome to Group 1",
-    },
-    null,
-    2
-    ),
-  };
+console.log('Loading function');
+
+export const handler = async (event, context) => {
+    return {
+        statusCode: 200,
+        body: JSON.stringify(
+            {
+                message: "Hello, World!",
+            },
+          null,
+          2
+        ),
+    };
 };
