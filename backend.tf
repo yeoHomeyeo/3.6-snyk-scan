6550eae91066445ddc482a78af383a97127f0494
Indigo_cE9 <chrisyeohc@outlook.com>
2025-05-13 00:03:17 +0800

    snyk correct error6

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
-# Comment out the below if you are working on local
+# # Comment out the below if you are working on local
 
-terraform {
-  backend "s3" {
-    bucket = "sctp-ce9-tfstate"
-    key    = "chrisy-snyk-scan.tfstate" #Change the value of this to <your suggested name>.tfstate for  example
-    region = "us-east-1"
-  }
-}
+# terraform {
+#   backend "s3" {
+#     bucket = "sctp-ce9-tfstate"
+#     key    = "chrisy-snyk-scan.tfstate" #Change the value of this to <your suggested name>.tfstate for  example
+#     region = "us-east-1"
+#   }
+# }
